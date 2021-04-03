e28f4906cbd28e4982aad58f66ac94d1dc3ed9f5
patrick hrabos <phrabos@gmail.com>
2021-04-03 13:17:32 -0400

    create post route, pass test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,23 @@ describe('vs3-postgreSQL routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('adds a movie to db', async () => {
+    const data = await request(app)
+      .post('/api/v1/movies')
+      .send({ title: 'Ready Player One', year: '2018', img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcReCVBdW7CJXDYANByX-l2oPeH3k7wCui_hQm32VNSfSyZAc1DT' })
+    
+    const res = data.body
+
+    expect(res).toEqual(
+      {
+        id: expect.any(String),
+        title: 'Ready Player One',
+        year: '2018',
+        img: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcReCVBdW7CJXDYANByX-l2oPeH3k7wCui_hQm32VNSfSyZAc1DT'
+      }
+    )
+  })
 });
+
+
